57c5d559ef3ec26b9a2e554610b9948997df397f
syun17 <shunki.s777@gmail.com>
2024-10-28 21:37:22 +0900

    新規作成

--- main.js
+++ main.js
@@ -1,4 +1,41 @@
-//作成
-//読み込み
-//削除
-//更新
+const table = document.querySelector('table');
+const todo = document.getElementById('todo');
+const deadline = document.querySelector('input[type="date"]');
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', () =>{
+    const item = {};
+    item.todo = todo.value;
+    item.deadline = deadline.value;
+    item.done = false; 
+    console.log(item);
+    todo.value ='';
+    deadline.value = '';
+    const tr = document.createElement('tr'); 
+
+for (const prop in item) {
+  const td = document.createElement('td');
+  td.textContent = item[prop];
+  tr.appendChild(td);
+}
+
+table.append(tr);
+});
+
+for (const prop in item) {
+  const td = document.createElement('td');
+  td.textContent = item[prop];
+  tr.appendChild(td);
+}
+for (const prop in item) {
+  const td = document.createElement('td');
+  if (prop == 'done') {
+     const checkbox = document.createElement('input'); 
+     checkbox.type = 'checkbox'; 
+     checkbox.checked = item[prop]; 
+     td.appendChild(checkbox);
+  } else {
+    td.textContent = item[prop];
+  }
+  tr.appendChild(td);
+}
